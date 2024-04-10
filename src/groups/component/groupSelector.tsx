66f79c0f2d4bfbb323ef3a5829feecb95d729907
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-04-10 11:42:59 +0300

    [DOP-14004] Create Group selector

--- src/groups/component/groupSelector.tsx
+++ src/groups/component/groupSelector.tsx
@@ -1,58 +1,11 @@
 import {FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from "@mui/material";
-import {useStore} from "react-admin";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import useGetGroupList from "../../hooks/useGetGroupList";
 
-
-type GroupStateType = [
-    {id: number,
-    name: string,}
-]
 
 const Groups = () => {
-
-    const [currentUserToken, setCurrentUserToken] = useState('')
-    const [, setCurrentGroup] = useStore("currentGroup", '');
-    const [currentGroupList, setCurrentGroupList] = useState<GroupStateType | []>([]);
     const [selectorValue, setSelectorValue] = useState('');
-
-    useEffect(
-        () => {
-            const formdata = new FormData();
-            // TODO: replace with getting username and password from authorization
-            formdata.append("username", "not_empty_group_member_maintainer");
-            formdata.append("password", "password");
-            const requestOptions = {
-                method: 'POST',
-                body: formdata,
-                redirect: 'follow'
-            };
-            fetch("http://localhost:8000/v1/auth/token", requestOptions)
-                .then(res => res.json())
-                .then(json => json.access_token)
-                .then(setCurrentUserToken)
-                .catch(e => console.log(e));
-
-
-        }, []
-    )
-
-    useEffect(() => {
-        const myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer " + currentUserToken);
-
-        const requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
-        };
-        if (currentUserToken) {
-            fetch("http://localhost:8000/v1/groups", requestOptions)
-                .then(res => res.json())
-                .then(json => json.items)
-                .then(setCurrentGroupList)
-                .catch(e => console.log(e));
-        }
-    }, [currentUserToken]);
+    const {currentGroupList, setCurrentGroup} = useGetGroupList()
 
     const handleChange = (event: SelectChangeEvent, child) => {
         setCurrentGroup(child.props.children)
