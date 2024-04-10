8d50fe4922d5d5785ff5f7b656f87df4677f81d2
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-04-10 12:15:51 +0300

    [DOP-14004] Create Group selector

--- src/groups/component/groupSelector.tsx
+++ src/groups/component/groupSelector.tsx
@@ -1,38 +1,45 @@
-import {FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from "@mui/material";
-import {useState} from "react";
+import {
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+} from "@mui/material";
+import { useState } from "react";
 import useGetGroupList from "../../hooks/useGetGroupList";
 
-
 const Groups = () => {
-    const [selectorValue, setSelectorValue] = useState('');
-    const {currentGroupList, setCurrentGroup} = useGetGroupList()
+    const [selectorValue, setSelectorValue] = useState("");
+    const { currentGroupList, setCurrentGroup } = useGetGroupList();
 
     // @ts-ignore
     const handleChange = (event: SelectChangeEvent, child) => {
-        setCurrentGroup(child.props.children)
-        setSelectorValue(event.target.value as string)
+        setCurrentGroup(child.props.children);
+        setSelectorValue(event.target.value as string);
     };
 
     return (
         <>
-            <FormControl style={{minWidth: 150}}>
-                <InputLabel id="demo-simple-select-label">Select group</InputLabel>
-                <Select labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        label="Groups"
-                        onChange={handleChange}
-                        value={selectorValue}
+            <FormControl style={{ minWidth: 150 }}>
+                <InputLabel id="demo-simple-select-label">
+                    Select group
+                </InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Groups"
+                    onChange={handleChange}
+                    value={selectorValue}
                 >
-                    {
-                        currentGroupList.map(
-                            (item) => <MenuItem value={item.id} key={item.id}>{item.name}</MenuItem>
-                        )
-                    }
+                    {currentGroupList.map((item) => (
+                        <MenuItem value={item.id} key={item.id}>
+                            {item.name}
+                        </MenuItem>
+                    ))}
                 </Select>
             </FormControl>
-
         </>
-    )
-}
+    );
+};
 
-export default Groups
+export default Groups;
