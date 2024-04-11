add039fb8ce447fb32671d172f577b06f83d20c5
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-04-11 18:34:53 +0300

    [DOP-14004] Create Group selector

--- src/layout/AppBar.tsx
+++ src/layout/AppBar.tsx
@@ -1,28 +1,13 @@
 import * as React from "react";
-import { AppBar, TitlePortal, useDataProvider, useStore } from "react-admin";
+import { AppBar, TitlePortal } from "react-admin";
 import { Box, Theme, useMediaQuery } from "@mui/material";
 import { AppBarToolbar } from "./AppBarToolbar";
 import Groups from "../groups/component/groupSelector";
-import { useQuery } from "react-query";
-import { useEffect } from "react";
-import { GroupStateType } from "../types";
 
 const CustomAppBar = () => {
-    const token = localStorage.getItem("token");
-    const username = localStorage.getItem("username");
-    const dataProvider = useDataProvider();
-    const [_, setGroupList] = useStore<GroupStateType[]>("groupList", []);
     const isLargeEnough = useMediaQuery<Theme>((theme) =>
         theme.breakpoints.up("sm"),
     );
-    const { data, isLoading, error } = useQuery(
-        ["groups", username, token],
-        () => dataProvider.getGroupList(),
-    );
-
-    useEffect(() => {
-        if (data) setGroupList(data);
-    }, [token, username]);
 
     return (
         <AppBar color="secondary" toolbar={<AppBarToolbar />}>
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,23 @@
-import * as React from 'react';
-import { Layout, LayoutProps } from 'react-admin';
-import AppBar from './AppBar';
+import * as React from "react";
+import { Layout, LayoutProps, useDataProvider, useStore } from "react-admin";
+import AppBar from "./AppBar";
+import { GroupStateType } from "../types";
+import { useQuery } from "react-query";
+import { useEffect } from "react";
 
-export default (props: LayoutProps) => (
-    <Layout {...props} appBar={AppBar} />
-);
+export default (props: LayoutProps) => {
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+    const dataProvider = useDataProvider();
+    const [_, setGroupList] = useStore<GroupStateType[]>("groupList", []);
+    const { data, isLoading, error } = useQuery(
+        ["groups", username, token],
+        () => dataProvider.getGroupList(),
+    );
+
+    useEffect(() => {
+        if (data) setGroupList(data);
+    }, [token, username]);
+
+    return <Layout {...props} appBar={AppBar} />;
+};
