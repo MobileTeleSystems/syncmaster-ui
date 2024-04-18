eddaa0f3eb591ce8e7203f8ac3bf3ab6ff5b70a9
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-04-18 16:11:38 +0300

    [DOP-14005] Create Connections list page

--- src/shared/api/authProvider.ts
+++ src/shared/api/authProvider.ts
@@ -20,8 +20,7 @@ const authProvider: AuthProvider = {
             })
             .then((json) => json.access_token)
             .then((token) => {
-                localStorage.setItem("token", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHBpcmVzIjoxNzEzMzA4MTA1LjcxODAyMDd9.rLsvPsKIPSRVlFnriE_ddKgCvOugf633dIuYZ1bDKEU');
-                // localStorage.setItem("token", token);
+                localStorage.setItem("token", token);
                 localStorage.setItem("username", username);
             })
             .catch((e) => {
