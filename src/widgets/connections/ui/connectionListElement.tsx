2cedb8fd868b654ab603253a74f059f98650928c
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-05-09 02:32:45 +0300

    [DOP-14007] Implement Connection editing

--- src/widgets/connections/ui/connectionListElement.tsx
+++ src/widgets/connections/ui/connectionListElement.tsx
@@ -10,7 +10,15 @@ const ConnectionListElement = () => {
                 source="connection_data.type"
                 label={"Connection Type"}
             />
-            <DeleteButton mutationMode="pessimistic" />
+            <DeleteButton
+                mutationMode="pessimistic"
+                sx={{
+                    bgcolor: "background.paper",
+                    boxShadow: 1,
+                    borderRadius: 2,
+                    p: 2,
+                }}
+            />
         </Datagrid>
     );
 };
