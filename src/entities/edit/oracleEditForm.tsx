033db4df012af25fc5f8e8348752d9fd394a8b5c
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-05-09 14:01:59 +0300

    [DOP-14007] Implement Connection editing

--- src/entities/edit/oracleEditForm.tsx
+++ src/entities/edit/oracleEditForm.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { NumberInput, PasswordInput, TextInput } from "react-admin";
 
 const OracleEditForm = () => {
+    const [sidState, setSidState] = useState(false);
+    const [serviceNameState, setServiceNameState] = useState(false);
+    const [sidValue, setSidValue] = useState("");
+    const [serviceNameValue, setServiceNameValue] = useState("");
+
     return (
         <>
             <TextInput
                 name={"connection_data.sid"}
                 source="connection_data.sid"
                 label={"sid"}
+                value={sidValue}
+                onChange={(e) => {
+                    if (e.target.value !== "") {
+                        setServiceNameState(true);
+                    } else setServiceNameState(false);
+                    setSidValue(e.target.value);
+                }}
+                disabled={sidState}
             />
             <TextInput
                 source="connection_data.host"
@@ -17,6 +31,14 @@ const OracleEditForm = () => {
                 source="connection_data.service_name"
                 label={"Service name"}
                 name={"connection_data.service_name"}
+                value={serviceNameValue}
+                onChange={(e) => {
+                    if (e.target.value !== "") {
+                        setSidState(true);
+                    } else setSidState(false);
+                    setServiceNameValue(e.target.value);
+                }}
+                disabled={serviceNameState}
             />
             <NumberInput
                 source="connection_data.port"
