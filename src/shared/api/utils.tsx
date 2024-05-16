0747d318cf0c6eb0774ddc1b227ed0af98846916
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-05-16 18:15:05 +0300

    [DOP-15761] Fix bug (#5)
    * [DOP-15761] Fix bug

--- src/shared/api/utils.tsx
+++ src/shared/api/utils.tsx
@@ -1,7 +1,13 @@
-export const getHeaders = () => {
+export const getAuthHeaders = (): Headers => {
     const myHeaders = new Headers();
     const token: string | null = localStorage.getItem("token");
     if (token) myHeaders.append("Authorization", "Bearer " + token);
 
     return myHeaders;
+};
+
+export const getPOSTHeaders = (): Headers => {
+    let headers: Headers = getAuthHeaders();
+    headers.append("Content-Type", "application/json");
+    return headers;
 };
