63482693d3d6c198e0263ee2f14aa94d79c60f08
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-06-21 15:11:12 +0300

    [DOP-16647] Add Github actions

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -1,8 +1,6 @@
 export type ConnectionTypes = { id: string; name: string }[];
 export type UserRoles = "Guest" | "Owner" | "Maintainer" | "Developer";
+
 export const getApiUrl = () => {
     return "http://localhost:8000";
 };
-const _some_trash = "awddwa";
-
-console.log(_some_trash);
