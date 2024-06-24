7b7e778e1ffe10230a6813769613cb3a9a8d0715
DMITRIY PEDCHENKO <dypedchenk@mts.ru>
2024-06-24 18:50:27 +0300

    [DOP-17428] add local fonts (#20)
    * [DOP-17428] add local fonts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import alias from "@rollup/plugin-alias";
 import react from "@vitejs/plugin-react";
 import visualizer from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        webfontDownload([
+            "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
+            "https://fonts.googleapis.com/css2?family=Onest:wght@300;400;500;700&display=swap",
+            "https://fonts.googleapis.com/css2?family=Gabarito:wght@500;600;700;900&display=swap",
+            "https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400;500;600;700&display=swap",
+            "https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;600;700&display=swap",
+        ]),
         react(),
         alias(),
         visualizer({
