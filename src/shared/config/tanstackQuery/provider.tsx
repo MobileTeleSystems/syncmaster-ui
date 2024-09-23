873f4429858621a7457bde3f137d489fae0177a3
Zabilsya <kvcherniko@mts.ru>
2024-09-23 20:27:28 +0600

    [DOP-19625] add tanstack query devtools

--- src/shared/config/tanstackQuery/provider.tsx
+++ src/shared/config/tanstackQuery/provider.tsx
@@ -1,8 +1,14 @@
 import React, { PropsWithChildren } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { queryClient } from './instance';
 
 export const TanstackQueryProvider = ({ children }: PropsWithChildren) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
