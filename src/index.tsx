b6ac6e445e7cb5b588802e9ea4039e8823e57180
Zabilsya <kvcherniko@mts.ru>
2024-09-23 22:20:29 +0600

    [DOP-19625] add fonts and css loader for antd

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
+import 'antd/dist/antd.css';
+import '@app/styles/global.scss';
+
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
