feeb3d81f293314c1afed26674a1664b1efadeb4
Zablisya <kvcherniko@mts.ru>
2024-10-28 14:23:59 +0600

    [DOP-21015] fix after review

--- src/features/group/GroupUserList/index.tsx
+++ src/features/group/GroupUserList/index.tsx
@@ -12,13 +12,17 @@ export const GroupUserList = memo(({ group, onUpdateRowClick, onDeleteRowClick }
     return groupService.getGroupUsers({ ...params, id: group.data.id });
   };
 
+  const isRenderRowAction = () => {
+    return hasAccessByUserRole(UserRole.Owner, group.role);
+  };
+
   return (
     <ManagedTable
       queryKey={[GroupQueryKey.GET_GROUP_USERS, group.data.id]}
       queryFunction={handleGetGroupUsers}
       columns={GROUP_USER_LIST_COLUMNS}
-      isRenderUpdateRowAction={() => hasAccessByUserRole(UserRole.Owner, group.role)}
-      isRenderDeleteRowAction={() => hasAccessByUserRole(UserRole.Owner, group.role)}
+      isRenderUpdateRowAction={isRenderRowAction}
+      isRenderDeleteRowAction={isRenderRowAction}
       onUpdateRowClick={onUpdateRowClick}
       onDeleteRowClick={onDeleteRowClick}
       rowKey="id"
