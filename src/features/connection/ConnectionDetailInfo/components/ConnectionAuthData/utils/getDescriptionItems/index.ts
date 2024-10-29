868f09e5ac085b2f907f64ef5bd9f0438d239c9f
Zabilsya <kvcherniko@mts.ru>
2024-10-29 15:37:13 +0600

    [DOP-20043] fix descriptions of utils

--- src/features/connection/ConnectionDetailInfo/components/ConnectionAuthData/utils/getDescriptionItems/index.ts
+++ src/features/connection/ConnectionDetailInfo/components/ConnectionAuthData/utils/getDescriptionItems/index.ts
@@ -2,12 +2,7 @@ import { DescriptionItem } from '@shared/types';
 
 import { GetDescriptionItemsProps } from './types';
 
-/** Util for mapping data for Description component depends on connection type
- *
- * @param data - Connection auth data depends on connection type
- *
- * @returns - Array of description items data {@link DescriptionItem}
- */
+/** Util for mapping data for Description component depends on connection type */
 export const getDescriptionItems = ({ data }: GetDescriptionItemsProps): DescriptionItem[] => {
   switch (data.type) {
     case 's3':
