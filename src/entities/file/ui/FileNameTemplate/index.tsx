6c04b38ae596621e8be691e6cc1393dcbb5f08f1
Zabilsya <kvcherniko@mts.ru>
2025-02-24 21:43:44 +0600

    [DOP-22299] fix after review

--- src/entities/file/ui/FileNameTemplate/index.tsx
+++ src/entities/file/ui/FileNameTemplate/index.tsx
@@ -2,14 +2,15 @@ import { Form, Input } from 'antd';
 import React from 'react';
 import { validateFormFieldByPattern } from '@shared/utils';
 
-import { FILE_NAME_TEMPLATE_REGEXP, fileNamePlaceholder, fileNameTemplateTooltipText } from './constants';
+import { FILE_NAME_TEMPLATE_REGEXP, fileNamePlaceholder } from './constants';
+import { FileNameTemplateTooltipText } from './components';
 
 export const FileNameTemplate = () => {
   return (
     <Form.Item
       label="Filename template"
       normalize={(value) => (value.trim() === '' ? undefined : value)}
-      tooltip={fileNameTemplateTooltipText}
+      tooltip={<FileNameTemplateTooltipText />}
       name={['target_params', 'file_name_template']}
       rules={[
         {
