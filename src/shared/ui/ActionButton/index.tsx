6933fa2023f2c255f313d10466c4a5fb13dc3856
Zabilsya <kvcherniko@mts.ru>
2025-03-28 17:38:37 +0600

    [DOP-20892] fix after review

--- src/shared/ui/ActionButton/index.tsx
+++ src/shared/ui/ActionButton/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Button } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
@@ -7,7 +7,7 @@ import { ActionButtonProps } from './types';
 import { BUTTON_SETTINGS } from './constants';
 import classes from './styles.module.less';
 
-export const ActionButton = ({ actionType, to, className, children, ...props }: ActionButtonProps) => {
+export const ActionButton = memo(({ actionType, to, className, children, ...props }: ActionButtonProps) => {
   const { t } = useTranslation();
 
   const { displayText, ...buttonSettings } = BUTTON_SETTINGS[actionType];
@@ -27,4 +27,4 @@ export const ActionButton = ({ actionType, to, className, children, ...props }: 
   }
 
   return button;
-};
+});
