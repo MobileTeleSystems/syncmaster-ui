52b8b651cdb669a77c7432300bffd21eac987000
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-10-31 12:03:20 +0300

    [DOP-29496] Allow reading settings from config.yml

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+SYNCMASTER_CONFIG_FILE=${SYNCMASTER_CONFIG_FILE:-/app/config.yaml}
+SYNCMASTER__UI__API_BROWSER_URL_DEFAULT="http://localhost:8000"
+SYNCMASTER__UI__AUTH_PROVIDER_DEFAULT="dummyAuthProvider"
+
+if [[ -z "${SYNCMASTER__UI__API_BROWSER_URL}" ]]; then
+  if [[ -f "$SYNCMASTER_CONFIG_FILE" ]]; then
+    SYNCMASTER__UI__API_BROWSER_URL=$(yq --raw-output ".ui.api_browser_url // \"$SYNCMASTER__UI__API_BROWSER_URL_DEFAULT\"" "$SYNCMASTER_CONFIG_FILE")
+  else
+    SYNCMASTER__UI__API_BROWSER_URL="$SYNCMASTER__UI__API_BROWSER_URL_DEFAULT"
+  fi
+fi
+
+if [[ -z "${SYNCMASTER__UI__AUTH_PROVIDER}" ]]; then
+  if [[ -f "$SYNCMASTER_CONFIG_FILE" ]]; then
+    SYNCMASTER__UI__AUTH_PROVIDER=$(yq --raw-output ".ui.auth_provider // \"$SYNCMASTER__UI__AUTH_PROVIDER_DEFAULT\"" "$SYNCMASTER_CONFIG_FILE")
+  else
+    SYNCMASTER__UI__AUTH_PROVIDER="$SYNCMASTER__UI__AUTH_PROVIDER_DEFAULT"
+  fi
+fi
+
 cat <<EOF > /usr/share/nginx/html/env-config.js
 window.env = {
   API_URL: "${SYNCMASTER__UI__API_BROWSER_URL}",
