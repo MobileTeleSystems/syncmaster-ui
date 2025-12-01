af0a59125016cc6382a28a91c9bf03f767148f8f
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-01 11:33:58 +0300

    [DOP-29475] Add pattern check and placeholder for transfer filenameTemplate

--- src/entities/file/ui/FileNameTemplate/constants.ts
+++ src/entities/file/ui/FileNameTemplate/constants.ts
@@ -1,5 +1,3 @@
 /** Regexp to filename template, which must include `{extension}` and `{index}` placeholder (e.g. qwerty_{index}.{extension}) */
-export const FILE_NAME_TEMPLATE_REGEXP =
-  /^(?=.*\{index\})(?=.*\{extension\})([a-zA-Z0-9._\-]*\{(?:index|extension|run_created_at|run_id)\}[a-zA-Z0-9._\-]*)*$/;
-
-export const fileNamePlaceholder = '{run_created_at}_{index}.{extension}';
+export const FILE_NAME_TEMPLATE_PATTERN =
+  /^(?=.*\{index\})(?=.*\{extension\})([\w\d.\-]*\{(?:index|extension|run_created_at|run_id)\}[a-zA-Z0-9._\-]*)*$/iu;
--- src/entities/file/ui/FileNameTemplate/index.tsx
+++ src/entities/file/ui/FileNameTemplate/index.tsx
@@ -1,28 +1,28 @@
 import { Form, Input } from 'antd';
 import React from 'react';
-import { validateFormFieldByPattern } from '@shared/utils';
 import { useTranslation } from 'react-i18next';
 
-import { FILE_NAME_TEMPLATE_REGEXP, fileNamePlaceholder } from './constants';
+import { FILE_NAME_TEMPLATE_PATTERN } from './constants';
 import { FileNameTemplateTooltipText } from './components';
 
 export const FileNameTemplate = () => {
-  const { t } = useTranslation('error');
+  const { t } = useTranslation('file');
 
   return (
     <Form.Item
-      label={t('filenameTemplate', { ns: 'file' })}
-      normalize={(value) => (value.trim() === '' ? undefined : value)}
+      label={t('filenameTemplate')}
       tooltip={<FileNameTemplateTooltipText />}
       name={['target_params', 'file_name_template']}
       rules={[
         {
-          pattern: FILE_NAME_TEMPLATE_REGEXP,
-          validator: (rule, value) => validateFormFieldByPattern(rule, value, t),
+          type: 'string',
+          required: true,
+          pattern: FILE_NAME_TEMPLATE_PATTERN,
+          message: t('invalidFilenameTemplate'),
         },
       ]}
     >
-      <Input className="nodrag" size="large" placeholder={fileNamePlaceholder} />
+      <Input className="nodrag" size="large" placeholder="{run_created_at}_{index}.{extension}" />
     </Form.Item>
   );
 };
