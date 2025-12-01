4d52c2aad371d5c5fe6ade4a3685e3bb26eadabf
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-01 11:33:08 +0300

    [DOP-29475] Add pattern check and placeholder for transfer directoryPath

--- src/entities/transfer/ui/TransferConnectionFileType/index.tsx
+++ src/entities/transfer/ui/TransferConnectionFileType/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Divider, Form, Input } from 'antd';
-import { ABSOLUTE_PATH_REGEXP } from '@shared/constants';
+import { ABSOLUTE_PATH_PATTERN } from '@shared/constants';
 import { FileFormatParams, FileNameTemplate } from '@entities/file/@x/transfer';
 import { useTranslation } from 'react-i18next';
 
@@ -14,9 +14,16 @@ export const TransferConnectionFileType = ({ name }: TransferConnectionFileTypeP
       <Form.Item
         label={t('directoryPath')}
         name={[name, 'directory_path']}
-        rules={[{ required: true, pattern: ABSOLUTE_PATH_REGEXP }]}
+        rules={[
+          {
+            type: 'string',
+            required: true,
+            pattern: ABSOLUTE_PATH_PATTERN,
+            message: t('invalidDirectoryPath'),
+          },
+        ]}
       >
-        <Input className="nodrag" size="large" />
+        <Input className="nodrag" size="large" placeholder="/path/to/directory" />
       </Form.Item>
       {name === 'target_params' && <FileNameTemplate />}
       <Divider>{t('fileFormatSettings', { ns: 'file' })}</Divider>
--- src/shared/constants/regexp.ts
+++ src/shared/constants/regexp.ts
@@ -1,5 +1,5 @@
 /** Regexp like absolute path in unix and windows OS (e.g. /home) */
-export const ABSOLUTE_PATH_REGEXP = /^(?:[a-zA-Z]:\\|\/)/;
+export const ABSOLUTE_PATH_PATTERN = /^\/[\w\d ]+[\w\d \/]*/iu;
 
 /** Regexp to table name (e.g. schema.table) */
 export const TABLE_NAME_PATTERN = /^[\w\d]+\.[\w\d]+$/iu;
