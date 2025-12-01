8cd4764ba36e4e00c99fc201bf2a93f5a808ef6a
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-01 12:14:06 +0300

    [DOP-29475] Fix filenameTemplate pattern

--- src/entities/file/ui/FileNameTemplate/constants.ts
+++ src/entities/file/ui/FileNameTemplate/constants.ts
@@ -1,3 +1,3 @@
 /** Regexp to filename template, which must include `{extension}` and `{index}` placeholder (e.g. qwerty_{index}.{extension}) */
 export const FILE_NAME_TEMPLATE_PATTERN =
-  /^(?=.*\{index\})(?=.*\{extension\})([\w\d.\-]*\{(?:index|extension|run_created_at|run_id)\}[a-zA-Z0-9._\-]*)*$/iu;
+  /^(?=.*\{index\})(?=.*\{extension\})([\w\d.\-]*\{(?:index|extension|run_created_at|run_id)\}[\w\d.\-]*)*$/iu;
