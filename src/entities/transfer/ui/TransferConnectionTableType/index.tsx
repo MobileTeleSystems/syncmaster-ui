fb9d9fd561bd19bf9fdd3c040ca12ef49121092e
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-01 11:31:46 +0300

    [DOP-29475] Add pattern check and placeholder for transfer tableName

--- src/entities/transfer/ui/TransferConnectionTableType/index.tsx
+++ src/entities/transfer/ui/TransferConnectionTableType/index.tsx
@@ -3,13 +3,18 @@ import { Form, Input } from 'antd';
 import { useTranslation } from 'react-i18next';
 
 import { TransferConnectionTableTypeProps } from './types';
+import { TABLE_NAME_PATTERN } from '@shared/constants';
 
 export const TransferConnectionTableType = ({ name }: TransferConnectionTableTypeProps) => {
   const { t } = useTranslation('transfer');
 
   return (
-    <Form.Item label={t('tableName')} name={[name, 'table_name']} rules={[{ required: true }]}>
-      <Input className="nodrag" size="large" />
+    <Form.Item
+      label={t('tableName')}
+      name={[name, 'table_name']}
+      rules={[{ type: 'string', required: true, pattern: TABLE_NAME_PATTERN, message: t('invalidTableName') }]}
+    >
+      <Input className="nodrag" size="large" placeholder="myschema.mytable" />
     </Form.Item>
   );
 };
--- src/shared/constants/regexp.ts
+++ src/shared/constants/regexp.ts
@@ -1,6 +1,9 @@
 /** Regexp like absolute path in unix and windows OS (e.g. /home) */
 export const ABSOLUTE_PATH_REGEXP = /^(?:[a-zA-Z]:\\|\/)/;
 
+/** Regexp to table name (e.g. schema.table) */
+export const TABLE_NAME_PATTERN = /^[\w\d]+\.[\w\d]+$/iu;
+
 /** Regexp to input only digits and digits with fractional part (e.g. 123.22) */
 export const NUMBER_REGEXP = /\d*\.?\d+$/;
 
