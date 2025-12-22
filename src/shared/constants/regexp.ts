8bcf06facab4d6f82338ffebe709a9ae807f80e0
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2025-12-22 11:13:21 +0300

    [DOP-29475] Allow nested namespaces in Iceberg table name

--- src/shared/constants/regexp.ts
+++ src/shared/constants/regexp.ts
@@ -1,5 +1,5 @@
 /** Regexp like absolute path in unix and windows OS (e.g. /home) */
 export const ABSOLUTE_PATH_PATTERN = /^\/[\w\d ]+[\w\d \/]*/iu;
 
-/** Regexp to table name (e.g. schema.table) */
-export const TABLE_NAME_PATTERN = /^[\w\d]+\.[\w\d]+$/iu;
+/** Regexp to table name (e.g. schema.table or namespace1.namespace2.table) */
+export const TABLE_NAME_PATTERN = /^[\w\d]+(\.[\w\d]+)+$/iu;
